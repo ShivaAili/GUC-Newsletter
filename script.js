d3a5043c3740a065392815b25494babec9d8a408
ShivaAili <158040429+ShivaAili@users.noreply.github.com>
2024-11-22 09:28:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const toggleBtn = document.querySelector(".toggle-btn");
+//     const moreContent = document.querySelector(".more-content");
+  
+//     toggleBtn.addEventListener("click", () => {
+//       if (moreContent.style.display === "none" || !moreContent.style.display) {
+//         moreContent.style.display = "block";
+//         toggleBtn.textContent = "View Less";
+//       } else {
+//         moreContent.style.display = "none";
+//         toggleBtn.textContent = "View More";
+//       }
+//     });
+//   });
+  
